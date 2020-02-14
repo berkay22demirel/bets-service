f6fbe83dfb6f23dc13d7682e905cd34df1ec19ea
berkay22demirel <Rj6x355pl>
2020-02-14 09:50:22 +0300

    Initial commit

---
+++ src/test/java/com/berkay22demirel/betsservice/BetsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.betsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
